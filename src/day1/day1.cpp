d0e9b6ccbc2d8369c64776f64b323a5963e81129
Olli Aalto <oaalto@gmail.com>
2023-12-29 12:33:29 +0200

    Add documentation.

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -1,3 +1,13 @@
+/**
+ * Advent of Code 2023 day 1.
+ * https://adventofcode.com/2023/day/1
+ *
+ * In part 1 the task is to find the first and last number from all the lines, then combine them into a number and sum
+ * all of them.
+ *
+ * In part 2 we notice that some of the numbers are actually spelled out and we have the recalculate the sum again.
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,6 +30,13 @@ static const std::map<std::string, std::string> NUMBER_MAP = {
 };
 
 namespace part1 {
+    /**
+     * Find the first and last number on the line and combine them into an integer.
+     * This function assumes that there is at least 1 number in the input string.
+     *
+     * @param line The input string
+     * @return The number gotten by combining the two numbers found.
+     */
     int parse_line(const std::string &line) {
         auto first = line.find_first_of(NUMBERS);
         auto last = line.find_last_of(NUMBERS);
@@ -35,6 +52,12 @@ namespace part1 {
 }
 
 namespace part2 {
+    /**
+     * Look for spelled out numbers, only the first and last on the line are added to the output vector.
+     *
+     * @param line The input string
+     * @param found_numbers The vector which contains the found numbers and their indices.
+     */
     void check_for_words(const std::string &line, std::vector<std::tuple<size_t, std::string>> &found_numbers) {
         for (auto &word: NUMBER_MAP) {
             auto first = line.find(word.first);
@@ -48,12 +71,13 @@ namespace part2 {
         }
     }
 
-    bool number_comparator(const std::tuple<size_t, std::string> &t1, const std::tuple<size_t, std::string> &t2) {
-        size_t first = get<0>(t1);
-        size_t second = get<0>(t2);
-        return first < second;
-    }
-
+    /**
+     * Find all the possible first and last numbers from the input, sort them and combine the first and last into
+     * the result number.
+     *
+     * @param line The input string
+     * @return The number combined from the first and last number, either spelled out or numeric.
+     */
     int parse_line(const std::string &line) {
         std::vector<std::tuple<size_t, std::string>> found_numbers;
 
@@ -69,6 +93,12 @@ namespace part2 {
 
         check_for_words(line, found_numbers);
 
+        auto number_comparator = [](const std::tuple<size_t, std::string> &t1,
+                                    const std::tuple<size_t, std::string> &t2) {
+            size_t first = get<0>(t1);
+            size_t second = get<0>(t2);
+            return first < second;
+        };
         sort(found_numbers.begin(), found_numbers.end(), number_comparator);
 
         auto first_num = found_numbers[0];
