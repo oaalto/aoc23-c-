53fd29f8e9f7928a7d43575cf4077ea979ada925
Olli Aalto <oaalto@gmail.com>
2023-12-29 13:56:17 +0200

    Move the documentation to the header file.

--- src/util.cpp
+++ src/util.cpp
@@ -1,14 +1,6 @@
 #include <fstream>
 #include "util.h"
 
-/**
- * Read the given file line by line and run them through the callback which parses a number from them. Finally return
- * a sum of the numbers to the caller.
- *
- * @param file_name The name of the input file
- * @param callback The callback which parses the input number from a line
- * @return All input numbers summed up.
- */
 int calculate_sum_from_file(const std::string &file_name, const std::function<int(const std::string &)> &callback) {
     int sum = 0;
 
@@ -20,12 +12,6 @@ int calculate_sum_from_file(const std::string &file_name, const std::function<in
     return sum;
 }
 
-/**
- * Read the file into memory line by line.
- *
- * @param file_name The name of the input file
- * @return A vector of strings containing the lines from the file.
- */
 std::vector<std::string> read_file(const std::string &file_name) {
     std::vector<std::string> lines;
     std::ifstream file(file_name);
--- src/util.h
+++ src/util.h
@@ -6,8 +6,22 @@
 #include <string_view>
 #include <vector>
 
+/**
+ * Read the given file line by line and run them through the callback which parses a number from them. Finally return
+ * a sum of the numbers to the caller.
+ *
+ * @param file_name The name of the input file
+ * @param callback The callback which parses the input number from a line
+ * @return All input numbers summed up.
+ */
 int calculate_sum_from_file(const std::string &, const std::function<int(const std::string &)> &);
 
+/**
+ * Read the file into memory line by line.
+ *
+ * @param file_name The name of the input file
+ * @return A vector of strings containing the lines from the file.
+ */
 std::vector<std::string> read_file(const std::string &);
 
 #endif //ADVENTOFCODE_UTIL_H
